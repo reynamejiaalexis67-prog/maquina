467c04e202106eed127a7f9644f606cb947a35a5
elpecao3444h <reynamejiaalexis67@gmail.com>
2026-03-24 11:02:05 -0700

    se agrego metodo para descontar inventario

--- entidades/snack.py
+++ entidades/snack.py
@@ -2,5 +2,22 @@ from enums.precio import TipoProducto
 
 class Maquina:
     
-    def __init__(self,nombre:str,precio:float,cantidad:int,tipo:Tipoproducto):
+    def __init__(self,nombre:str,precio:float,cantidad:int,tipo:TipoProducto):
+        
+        self.nombre=nombre
+        self.precio=precio
+        self.cantidad=cantidad
+        self.tipo=tipo
+        
+        
+        
+    def vender (self):
+        if self.cantidad>0:
+            
+        
+           self.cantidad-=1
+        else: 
+            print(" no se puede vender nada")
+        
+    
         
--- enums/precio.py
+++ enums/precio.py
@@ -2,6 +2,14 @@ from enum import Enum
     
 class  TipoProducto(Enum):
     
+    pan:22
+    
+    bebida:26
+    
+    golosina:15
+    
+    SABRITAS:30
+    
   
        
        
